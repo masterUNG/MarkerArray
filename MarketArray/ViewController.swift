5fed60ff68c11baa010deb09397b437b0939d2fe
MasterUNG <phrombutr@gmail.com>
2017-10-14 14:25:39 +0700

    Initial Commit

---
+++ MarketArray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarketArray
+//
+//  Created by MasterUNG on 14/10/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
